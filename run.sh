c99f85579b5fe174a81391b1639054573df72bd2
gadido30 <gadido30@gmail.com>
2014-10-27 18:49:28 +0200

    Adding more channles new

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#tv_grab_il  --config-file /home/user1/XMLTV/tv-il-radio-il/list-xml.txt --days 7 --slow  --output /home/user1/XMLTV/tv-il-radio-il/tt-list.xml
+tv_grab_il  --config-file /home/user1/XMLTV/tv-il-radio-il/list-xml.txt --days 7 --slow  --output /home/user1/XMLTV/tv-il-radio-il/tt-list.xml
 git --git-dir=/home/user1/XMLTV/tv-il-radio-il/.git add .
 git --git-dir=/home/user1/XMLTV/tv-il-radio-il/.git commit -am "Adding more channles new" 
 git --git-dir=/home/user1/XMLTV/tv-il-radio-il/.git push -u origin master
